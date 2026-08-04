dc5e14fa3477a68a8647594bc6904064d022b112
Alejandro Sojo <alejandrojsrv@gmail.com>
2026-08-04 16:13:20 -0300

    chore(lint): fix prettier formatting in adult profile port

--- src/households/application/adult-profile-ports/adult-profile-repository.port.ts
+++ src/households/application/adult-profile-ports/adult-profile-repository.port.ts
@@ -3,7 +3,10 @@ import { AdultProfileView } from '../adult-profile-models/adult-profile-view';
 export const ADULT_PROFILE_REPOSITORY = Symbol('AdultProfileRepository');
 
 export interface AdultProfileRepository {
-  findActiveByUserAndHousehold(userId: string, householdId: string): Promise<AdultProfileView | null>;
+  findActiveByUserAndHousehold(
+    userId: string,
+    householdId: string,
+  ): Promise<AdultProfileView | null>;
   findActiveById(profileId: string): Promise<AdultProfileView | null>;
   listActiveByHousehold(householdId: string): Promise<AdultProfileView[]>;
 }
